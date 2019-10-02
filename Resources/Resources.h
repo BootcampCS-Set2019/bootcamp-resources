cfcc6c678ec2020542262dfab5f8d2bc29efe692
Matheus Bispo <matheus.filipe.bispo@accenture.com>
2019-10-02 08:25:31 -0300

    Add initial project files

---
+++ Resources/Resources.h
@@ -0,0 +1,19 @@
+//
+//  Resources.h
+//  Resources
+//
+//  Created by matheus.filipe.bispo on 02/10/19.
+//  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Resources.
+FOUNDATION_EXPORT double ResourcesVersionNumber;
+
+//! Project version string for Resources.
+FOUNDATION_EXPORT const unsigned char ResourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Resources/PublicHeader.h>
+
+
