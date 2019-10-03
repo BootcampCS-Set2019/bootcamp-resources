8716c319be85c968fe0dc154869c5de761b7dc4b
cs-bruno-carvalho <bruno.c.carvalho@accenture.com>
2019-10-03 13:25:55 -0300

    Add basic files for design system (Colors, Typography)

---
+++ Resources/DesignSystem/DesignSystem.swift
@@ -0,0 +1,9 @@
+//
+//  DesignSystem.swift
+//  Resources
+//
+//  Created by bruno.c.carvalho on 03/10/19.
+//  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
+//
+
+import Foundation
---
+++ Resources/DesignSystem/Typography.swift
@@ -0,0 +1,9 @@
+//
+//  Typography.swift
+//  Resources
+//
+//  Created by bruno.c.carvalho on 03/10/19.
+//  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
+//
+
+import Foundation
