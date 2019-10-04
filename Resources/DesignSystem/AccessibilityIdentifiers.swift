35fe92746fb2718dcf565e8bd8f25b1825312641
Bruno <bruno.c.carvalho@accenture.com>
2019-10-04 18:57:41 -0300

    Add accessibility identifier resource

--- Resources/DesignSystem/AccessibilityIdentifiers.swift
+++ Resources/DesignSystem/AccessibilityIdentifiers.swift
@@ -10,7 +10,32 @@ import Foundation
 import UIKit
 
 extension MagicDesignSystem {
-    public enum AccessibilityIdentifiers {
-        
+    public struct AccessibilityIdentifiers {
+        private(set) var constructedName: String = ""
+
+        public init(componentType: ComponentTypes, module: ModuleNames, number: Int?) {
+            self.constructedName += componentType.rawValue + module.rawValue
+            if let number = number {
+                self.constructedName += String(number)
+            }
+        }
     }
+}
+
+public enum ComponentTypes: String {
+    case label = "lbl"
+    case button = "btn"
+    case text = "txt"
+    case view = "view"
+    case imageView = "img"
+    case tableView = "tbl"
+    case tableViewCell = "tblCell"
+    case collectionView = "collec"
+    case collectionViewCell = "collecCell"
+}
+
+public enum ModuleNames: String {
+    case recommendedCards = "RecommendedCards"
+    case cardDetail = "CardDetail"
+    case favoredCards = "FavoredCards"
 }
